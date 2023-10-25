a42e864f22ba2ce1c5154d7c660e46d4372bc5b8
Antoni Arif <antonisantiang@gmail.com>
2023-10-25 10:20:24 +0700

    we

--- views.py
+++ views.py
@@ -8,16 +8,16 @@ from bs4 import BeautifulSoup
 data = []
 
 # modified
-# @App.route("/", methods=["GET", "POST"])
-# def sample_api():
-#     if request.method == "GET":
-#         data.append('wndi')
-#     return json.dumps(data)
-
-@App.route('/', methods=["GET", "POST"])
-def getInfoSumbar():
+@App.route("/", methods=["GET", "POST"])
+def sample_api():
     if request.method == "GET":
-        last_page_num = 2
+        data.append('wndi')
+    return json.dumps(data)
+
+@App.route('/infosumbar', methods=["GET", "POST"])
+def getInfoSumbar():
+    if request.method == "POST":
+        last_page_num = request.json['pagination']
         total_words = []
         for i in range(1,int(last_page_num+1)):
             r = requests.get("https://infosumbar.net/page/"+format(i)+"/?s=padang+panjang")
@@ -32,6 +32,6 @@ def getInfoSumbar():
                 #author = word.find(attrs={'class': 'jeg_post_meta'}).find(attrs={'class': 'jeg_meta_author'}).a.text
                 data={'url' : url,'tgl' : tgl}
                 total_words.append(data)
-    #hasil = {"total" : len(total_words), "data" : total_words}
-    return json.dumps(total_words)
+    hasil = {"total" : len(total_words), "data" : total_words}
+    return json.dumps(hasil)
    
