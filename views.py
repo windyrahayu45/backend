de130e46a3cce0252fc3c2a58816089edbe5bd55
Antoni Arif <antonisantiang@gmail.com>
2023-10-25 11:06:55 +0700

    we

--- views.py
+++ views.py
@@ -34,4 +34,23 @@ def getInfoSumbar():
                 total_words.append(data)
     hasil = {"total" : len(total_words), "data" : total_words}
     return jsonify(hasil)
+
+@App.route('/antara', methods=["GET", "POST"])
+def getAntara():
+    if request.method == "GET":
+        startDate = '2023-01-01'
+        endDate = '2023-12-31'
+        total_words = []
+        output = []
+        r = requests.get("https://www.antaranews.com/search?q=padang+panjang&startDate="+format(startDate)+"&endDate="+format(endDate)+"&inTitle=1&submit=Submit")
+        soup = BeautifulSoup(r.content.lower(), 'html.parser')
+        words = soup.findAll('article',{"class" : "simple-post simple-big clearfix"} )
+        count = len(words)
+        for word in words:
+            url = word.find(attrs={'class': "simple-thumb"}).a['href']
+            tgl = word.find('header').span.text
+            data={'url' : url,'tgl':tgl}
+            total_words.append(data)
+    hasil = {"total" : len(total_words), "data" : total_words}
+    return jsonify(hasil)
    
