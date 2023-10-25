8796aea8dbc081af3719454afdfc66bb4c6978f3
Antoni Arif <antonisantiang@gmail.com>
2023-10-25 10:04:03 +0700

    infsumbar

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from Cors import App 
-from flask import request
+from flask import request, jsonify
 import json
+import requests
+from bs4 import BeautifulSoup
 
 
 data = []
@@ -12,5 +14,27 @@ def sample_api():
         data.append('wndi')
     return json.dumps(data)
 
+@App.route('/infosumbar', methods=["GET", "POST"])
+def getInfoSumbar():
+    if request.method == "POST":
+        last_page_num = request.json['pagination']
+        total_words = []
+        output = []
+        for i in range(1,int(last_page_num+1)):
+            r = requests.get("https://infosumbar.net/page/"+format(i)+"/?s=padang+panjang")
+            soup = BeautifulSoup(r.content.lower(), 'html.parser')
+            words = soup.findAll('article')
+            count = len(words)
 
+            for word in words:
+                url = word.find(attrs={'class': 'jeg_thumb'}).a['href']
+                title = word.find(attrs={'class': 'jeg_postblock_content'}).a.text
+                berita = word.find(attrs={'class': 'jeg_post_excerpt'}).p.text
+                tgl = word.find(attrs={'class': 'jeg_post_meta'}).find(attrs={'class': 'jeg_meta_date'}).a.text
+                author = word.find(attrs={'class': 'jeg_post_meta'}).find(attrs={'class': 'jeg_meta_author'}).a.text
+
+                data={'url' : url,'tgl' : tgl}
+                total_words.append(data)
+    hasil = {"total" : len(total_words), "data" : total_words}
+    return jsonify(hasil)
    
