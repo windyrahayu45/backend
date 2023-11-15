9d6d560ad33609aaba07b0f28680d99deb33111e
windyrahayu45 <windyrahayuuu@gmail.com>
2023-11-15 09:56:51 +0700

    investigasi

--- views.py
+++ views.py
@@ -148,3 +148,25 @@ def getFajarSumbar():
             total_words.append(data)
     hasil = {"total" : len(total_words), "data" : total_words}
     return jsonify(hasil)
+
+
+@App.route('/investigasi', methods=["GET", "POST"])
+def getInvestasi():
+    if request.method == "GET":
+        last_page_num = request.args['pagination']
+        total = int(last_page_num)
+        total_words = []
+        for i in range(1,total):
+            url = "https://investigasi.news/page/"+format(i)+"/?s=padang+panjang"
+            headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"}
+            webpage = requests.get(url,headers=headers)
+            soup = BeautifulSoup(webpage.content, 'html.parser')
+            words = soup.findAll('div',{"class" : "td-block-span6"} )
+
+            for word in words:
+                url = word.find(attrs={'class': 'entry-title td-module-title'}).a['href']
+                tgl = word.find(attrs={'class': 'td-post-date'}).get_text()
+                data={'url' : url,'tgl' : tgl}
+                total_words.append(data)
+    hasil = {"total" : len(total_words), "data" : total_words}
+    return jsonify(hasil)
