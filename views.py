8ceeff1ca1fc03d46b3c4c7281c6f8d3739b73a2
windyrahayu45 <windyrahayuuu@gmail.com>
2023-11-16 08:22:19 +0700

    klikpositif

--- views.py
+++ views.py
@@ -374,3 +374,25 @@ def getDutaMetro():
                 total_words.append(data)
     hasil = {"total" : len(total_words), "data" : total_words}
     return jsonify(hasil)
+
+
+@App.route('/klikpositif', methods=["GET", "POST"])
+def getKlikPositif():
+    if request.method == "GET":
+        last_page_num = request.args['pagination']
+        total = int(last_page_num)
+        total_words = []
+        for i in range(1,total):
+            url = "https://klikpositif.com/page/"+format(i)+"/?s=padang+panjang"
+            headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"}
+            webpage = requests.get(url,headers=headers)
+            soup = BeautifulSoup(webpage.content, 'html.parser')
+            words = soup.findAll('article')
+
+            for word in words:
+                url = word.find(attrs={'class': 'jeg_post_title'}).a['href']
+                tgl = word.find(attrs={'class': 'jeg_meta_date'}).a.text
+                data={'url' : url,'tgl' : tgl}
+                total_words.append(data)
+    hasil = {"total" : len(total_words), "data" : total_words}
+    return jsonify(hasil)
