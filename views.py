d086817386941b57ba805223552001c4345018f5
windyrahayu45 <windyrahayuuu@gmail.com>
2023-11-16 08:12:32 +0700

    minangkabaunews

--- views.py
+++ views.py
@@ -261,3 +261,25 @@ def getPadangMedia():
                 total_words.append(data)
     hasil = {"total" : len(total_words), "data" : total_words}
     return jsonify(hasil)
+
+
+@App.route('/minangkabaunews', methods=["GET", "POST"])
+def getMinangkabauNews():
+    if request.method == "GET":
+        last_page_num = request.args['pagination']
+        total = int(last_page_num)
+        total_words = []
+        for i in range(1,total):
+            url = "https://minangkabaunews.com/page/"+format(i)+"/?s=padang+panjang"
+            headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"}
+            webpage = requests.get(url,headers=headers)
+            soup = BeautifulSoup(webpage.content, 'html.parser')
+            words = soup.findAll('article')
+
+            for word in words:
+                url = word.find(attrs={'class': 'entry-title'}).a['href']
+                tgl = word.find(attrs={'class': 'posted-on byline'}).get_text()
+                data={'url' : url,'tgl' : tgl}
+                total_words.append(data)
+    hasil = {"total" : len(total_words), "data" : total_words}
+    return jsonify(hasil)
